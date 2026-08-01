bb8698cdb4edc2ea63b6357a780158a22f77c582
zhugedalu <1801821343@qq.com>
2026-08-01 22:32:46 +0800

    新增了字符串f_format快速格式化。

--- 18_字符串f_format格式化.py
+++ 18_字符串f_format格式化.py
@@ -2,6 +2,7 @@
 使用f_format方式格式化字符串（格式化就是字符串拼接   ）
 语法: f"内容{变量}"
 好处：不理会类型（全部作为字符串用）、不做精度控制。
+     做精度控制的话 f"内容{变量：精度}"
 """
 name = "蔡小葵"
 age = 19
@@ -10,3 +11,4 @@ height = 190.66
 # 变量用{}括起来
 info =  f"我叫{name},今年{age}岁，身高为{height}cm"
 print( f"我叫{name},今年{age}岁，身高为{height}cm")
+print( f"我叫{name},今年{age}岁，身高为{height:8.1f}cm")
