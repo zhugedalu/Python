63397ffd6c03c47bf0985e33721f29cb2f1f664c
zhugedalu <1801821343@qq.com>
2026-08-15 23:33:20 +0800

    新增了循环中断continue和break。

---
+++ 03循环语句/13_循环中断.py
@@ -0,0 +1,35 @@
+"""
+无论是while循环还是for循环，都是重复性的执行特定操作。
+在这个重复过程中，会出现一些其它情况让我们不得不：
+1. 暂时跳过某次循环，直接进行下一次。
+2. 提前退出循环，不再继续。
+
+对于这种场景，python提供 continue 和 break 关键字用以对循环进行临时跳过和直接结束。
+"""
+# continue
+# continue关键字用于：中断本次循环，直接进入下一轮循环。
+# continue可以用于：for循环和while循环，效果一致。
+# for i in range(1,100):
+#     语句 1
+#     continue
+#     语句2
+# 在循环内，遇到continue就结束本次循环，进行下一次。所以，语句2是不会执行的。
+# 应用场景：在循环中，因某些原因，临时结束本次循环。
+for i in range(3):
+    print("have good luck!")
+    continue
+    print("see you soon")
+# continue关键字只可以控制：它所在的循环临时中断。不能越级，涉及到嵌套
+
+
+# break
+# break关键字用于：直接结束循环
+# break可以用于：for循环和while循环，效果一致。
+for i in range(3):
+    print("good night!")                    # 语句1
+    break
+    print("see you soon")                   # 语句2
+    print(" i hate you")                    # 语句3
+# 在循环内，遇到break就结束循环了
+# 所以，执行了语句1后，直接执行语句3
+# break关键字同样只可以控制：它所在的循环结束
