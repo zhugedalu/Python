8c95c35746862d535d74b7ecbc52046c7f066c7e
zhugedalu <1801821343@qq.com>
2026-08-16 23:03:25 +0800

    新增了循环中断continue。

--- 03循环语句/13_continue关键字.py
+++ 03循环语句/13_continue关键字.py
@@ -15,21 +15,28 @@
 #     语句2
 # 在循环内，遇到continue就结束本次循环，进行下一次。所以，语句2是不会执行的。
 # 应用场景：在循环中，因某些原因，临时结束本次循环。
-for i in range(3):
-    print("have good luck!")
-    continue
-    print("see you soon")
-# continue关键字只可以控制：它所在的循环临时中断。不能越级，涉及到嵌套
+# for i in range(3):
+#     print("have good luck!")
+#     continue
+#     print("see you soon")
+# continue关键字只可以控制：它所在的循环临时中断。不能越级(只对所在循环有效，对上层循环没用)，涉及到嵌套。
 
 
 # break
 # break关键字用于：直接结束循环
 # break可以用于：for循环和while循环，效果一致。
-for i in range(3):
-    print("good night!")                    # 语句1
-    break
-    print("see you soon")                   # 语句2
-    print(" i hate you")                    # 语句3
+# for i in range(3):
+#     print("good night!")                    # 语句1
+#     break
+#     print("see you soon")                   # 语句2
+#     print(" i hate you")                    # 语句3
 # 在循环内，遇到break就结束循环了
 # 所以，执行了语句1后，直接执行语句3
 # break关键字同样只可以控制：它所在的循环结束
+
+# 有10碗饭，内容不一样，每吃一碗前都会问你，这个吃不吃，吃就吃掉，不吃就跳过。
+for i in range(1,11):
+    flag = int(input(f"这是第{i}碗饭，吃还是不吃？(吃输入1，不吃输入0)"))
+    if flag == 0:
+        continue            # continue尽管在if内，但是和if没有任何关联，continue仅对for或while有效
+    print(f"正在吃第{i}碗饭，嗷呜嗷呜吃完啦.....")
